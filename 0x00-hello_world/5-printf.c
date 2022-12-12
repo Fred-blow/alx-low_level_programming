7b18793e5dae1777f568d806d8cfdf913d3db056
-m <-m>
2022-12-12 03:33:12 -0800

    ProjectC

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 # include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
 int main(void)
-{       printf("With proper grammar, but the outcome is a piece of art,");
+{       printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
